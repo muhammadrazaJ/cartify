112fc1cbd3df3d4b715cc4185df46922884ccecb
Muhammad Raza <raza.jessani@yahoo.com>
2026-02-14 19:09:56 +0500

    First Commit

---
+++ src/test/java/com/cartify/cartify/CartifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartify.cartify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
